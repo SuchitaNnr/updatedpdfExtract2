77ded17895e9af9e7f509fb85b4039e0aab7ea0a
SuchitaNnr <108674892+SuchitaNnr@users.noreply.github.com>
2022-07-11 14:11:37 +0100

    Add files via upload

---
+++ log.py
@@ -0,0 +1,14 @@
+import logging
+from logging.config import dictConfig
+import datetime
+class Log:
+   def __init__(self):
+        date = datetime.date.today()
+        file=str(date)+'_example.log'
+        #print(file)
+        logging.basicConfig(
+            filename=file,
+            level=logging.ERROR,
+            format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S',
+            )
